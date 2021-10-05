116905afbcd3bef5ea2e90e82b6cb53e9c2847dc
renan04lima <renan04lima@outlook.com>
2021-10-05 00:37:57 -0300

    feat: add JwtAuthGuard in transactions controller

--- nest-api/src/transactions/transactions.controller.ts
+++ nest-api/src/transactions/transactions.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { JwtAuthGuard } from './../auth/jwt-auth.guard';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
+
+@UseGuards(JwtAuthGuard)
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
